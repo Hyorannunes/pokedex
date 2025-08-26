d76fa957477f718beea1baabc978eb3fbcf86283
Hyoran <hyoran2000@gmail.com>
2025-08-25 21:46:41 -0300

    fix: correcao da configurcao do jest

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,15 +1,20 @@
 /** @type {import('jest').Config} */
 module.exports = {
   testEnvironment: "jsdom",
+  roots: ["<rootDir>/src"],
   setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
+
   transform: {
     "^.+\\.[jt]sx?$": "babel-jest",
   },
+
   moduleNameMapper: {
-    // Mock de estilos (evita erro ao importar .css/.scss etc.)
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-    // Mock de arquivos estáticos (imagens, fontes…)
     "\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
   },
-  testMatch: ["**/?(*.)+(test|spec).[jt]sx?"],
+
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(test|spec).[jt]s?(x)",
+  ],
 };
